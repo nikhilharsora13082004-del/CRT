dbbe1b6fc34aa6e82924b523ba54e92209a4128d
nikhilharsora13082004-del <nikhilharsora13082004@gmail.com>
2026-05-02 17:21:06 +0530

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,5 +1,15 @@
+import Java.Util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
-
+        Scanner scanner=new Scanner(System.in);
+System.out.println("Enter how many digits you want to print");
+        int n=scanner.nextInt();
+        int t1=0,t2=1;
+        int sum;
+        for(int i=1;i<=n;i++){
+            System.out.print(t1);
+            sum=t1+t2;
+            t1=t2;
+            t2=sum;
     }
 }
